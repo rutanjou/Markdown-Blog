c95f025d9675eed31a92732c59d6ea039af69834
Ru Tan <jutan2012@gmail.com>
2016-10-25 15:45:56 +0200

    Ajout etape 2

--- js/app.js
+++ js/app.js
@@ -1,21 +1,35 @@
 (function(){
 	"use strict";
 	var app = {
-		url:"http://192.168.1.40:1337/alice.md",
+		urlalice:"http://192.168.1.40:1337/alice.md",
+		urljson:"http://192.168.1.40:1337/menu.json",
 		init:function(){
-			$.ajax(app.url)
+			$.ajax(app.urlalice)
 			.done(app.ajaxDone)
 			.fail()
 			.always();
+			$.ajax(app.urljson)
+			.done(app.getjson)
+			.fail()
+			.always();
+
+
 		},
 	
 		ajaxDone:function(alice){
 			var converter = new showdown.Converter();
 			var html = converter.makeHtml(alice);
 			$("#md").html(html);
-		}
+		},
+		getjson:function(menu){
+			var url = "192.168.1.40:1337/menu.json";
+			var menus = menu.menu;
+			for(var i=0; i<menus.length;i++){
+				$("#menu").append("<li>" + "<a href= + url>" + menus[i].path + menus[i].title + "</li>");
+			}
+
 		
-		//ajaxDone:function(response){
+		}
         
 
 
